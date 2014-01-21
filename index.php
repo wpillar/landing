58d6abde2bba5a552b78211f899cd5ab73f36194
Will Pillar <will.pillar@graze.com>
2014-01-21 19:42:06 +0000

    Adding HTML5 boilerplate

--- index.php
+++ index.php
@@ -2,10 +2,31 @@
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+    'templates.path' => './views'
+));
 
-$app->get('/', function() {
-    echo 'hello!';
+/**
+ * Special Routes
+ */
+$app->notFound(function () use ($app) {
+    $app->render('404.html');
+});
+
+
+/**
+ * Routes
+ */
+$app->get('/', function() use ($app) {
+    $app->render('index.php');
+});
+
+
+/**
+ * Hooks
+ */
+$app->hook('slim.before', function () use ($app) {
+    $app->view()->appendData(array('baseUrl' => '../'));
 });
 
 $app->run();
